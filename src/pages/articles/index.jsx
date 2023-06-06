5463d808e73542ffb8455d74d0b9d733497ade98
William Pepple <williampepple1@gmail.com>
2023-06-06 12:44:16 +0100

    Update index.jsx

--- src/pages/articles/index.jsx
+++ src/pages/articles/index.jsx
@@ -41,12 +41,12 @@ export default function ArticlesIndex({ articles }) {
         <title>Articles - William Pepple</title>
         <meta
           name="description"
-          content="All of my long-form thoughts on programming, leadership, product design, and more, collected in chronological order."
+          content="Leetcod Miscellaneous Questions"
         />
       </Head>
       <SimpleLayout
-        title="Writing on fixing bugs and technical problems."
-        intro="All of my technical thoughts collected in chronological order."
+        title="Questions and Answers for selected leetcode questions" 
+        intro="Questions and Solutions"
       >
         <div className="md:border-l md:border-zinc-100 md:pl-6 md:dark:border-zinc-700/40">
           <div className="flex max-w-3xl flex-col space-y-16">
